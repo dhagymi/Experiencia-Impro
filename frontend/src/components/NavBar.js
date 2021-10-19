69e4fe3a1bc91d8f4cb9254ead3bacdd236d1031
agusand <amandreacchi@gmail.com>
2021-10-19 12:41:45 -0300

    Styles settings & fonts added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,36 +11,23 @@ const NavBar = () => {
 	];
 
 	return (
-		<nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto expansible">
-					{links.map(({ to, exact, innerText }) => {
-						return (
-							<li className="nav-item" key={to}>
-								<NavLink
-									className="nav-link type"
-									activeClassName="nav-link--active"
-									to={to}
-									exact={exact}
-								>
-									{innerText}
-								</NavLink>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
+		<nav className="navbar">
+			<ul className="navbar__list">
+				{links.map(({ to, exact, innerText }) => {
+					return (
+						<li className="navbar__listItem" key={to}>
+							<NavLink
+								className="navbar__link"
+								activeClassName="navbar__link navbar__link--active"
+								to={to}
+								exact={exact}
+							>
+								{innerText}
+							</NavLink>
+						</li>
+					);
+				})}
+			</ul>
 		</nav>
 	);
 };
