9cead28284217f4741ef471ad819d5515f667956
agusand <amandreacchi@gmail.com>
2021-10-19 19:30:34 -0300

    HomeTitleWord unused - Removed

--- frontend/src/components/HomeTitleWord.js
+++
@@ -1,9 +0,0 @@
-const HomeTitleWord = ({ children, direction }) => {
-	return (
-		<span className={`home__floatTitleWord home__floatTitleWord--${direction}`}>
-			{children}
-		</span>
-	);
-};
-
-export default HomeTitleWord;
