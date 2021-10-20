25c069072a2257fd2e01b0979bfe01f3846e14c2
agusand <amandreacchi@gmail.com>
2021-10-20 15:23:13 -0300

    Revert "Routing to home"
    This reverts commit aa002a3c6a13ae21062262ab49f2ea8712defcdf.

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import morgan from "morgan";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import history from "connect-history-api-fallback";
 
 import mailRouter from "./src/routers/mail.router.js";
 
@@ -19,14 +18,9 @@ app.set("PORT", PORT);
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(
-	history({
-		rewrites: [{ from: /\/.*/, to: "/" }],
-	})
-);
 
 // Public
-app.use("/", express.static(resolve(__dirname, "../frontend/build")));
+app.use(express.static(resolve(__dirname, "../frontend/build")));
 
 // Routes
 app.use("/api/mail", mailRouter);
