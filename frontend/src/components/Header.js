1a6653cf199dbfbb00cd79caaecb6ce40581340c
agusand <amandreacchi@gmail.com>
2021-10-25 19:05:47 -0300

    Header error fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,13 @@
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 import BurguerMenu from "./BurguerMenu";
+import { useMenuContext } from "../contexts/MenuContext";
 
 const Header = () => {
+	const { open } = useMenuContext();
+
 	return (
-		<header className="header">
+		<header className={`header ${open && "header--activedMenu"}`}>
 			<Logo />
 			<BurguerMenu />
 			<NavBar />
