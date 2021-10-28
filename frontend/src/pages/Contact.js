d42c57bc16fb316434b5b026cf2425e4b6722821
agusand <amandreacchi@gmail.com>
2021-10-28 18:47:30 -0300

    Contact section restyled

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -4,7 +4,7 @@ import ContactForm from "../components/ContactForm";
 const Contact = () => {
 	return (
 		<section className="contact">
-			<h1 className="contact__title">Contactanos</h1>
+			<h1 className="contact__title">Contacto</h1>
 			<div className="contact__body">
 				<ContactCard />
 				<ContactForm />
