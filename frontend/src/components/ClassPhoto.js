d0f84703c250e2a6aa5ba69f3e624d8e64cb4631
agusand <amandreacchi@gmail.com>
2021-11-03 09:42:34 -0300

    Photos animation optimized

--- frontend/src/components/ClassPhoto.js
+++ frontend/src/components/ClassPhoto.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, memo } from "react";
+import { useRef, memo } from "react";
 
 import useFloatingAnimation from "../hooks/useFloatingAnimation";
 
@@ -6,46 +6,15 @@ const ClassPhoto = ({
 	source,
 	alternative,
 	animationOptions,
-	onlyHover = true,
 	hoverClass = "",
 }) => {
-	const [isMouseOver, setIsMouseOver] = useState(false);
-	const [photoElement, setPhotoElement] = useState(null);
-
 	const photoReference = useRef();
-	const { current } = photoReference;
-
-	useEffect(() => {
-		if (!onlyHover) {
-			photoReference && setPhotoElement(current);
-			photoElement &&
-				window.addEventListener("mousemove", (event) => {
-					const mouseX = event.x;
-					const mouseY = event.y;
-
-					const { x, y, offsetHeight, offsetWidth } = photoElement;
-
-					if (
-						mouseX <= x + offsetWidth * 1.2 &&
-						mouseX >= x - offsetWidth * 0.2 &&
-						mouseY >= y - offsetHeight * 0.2 &&
-						mouseY <= y + offsetHeight * 1.2
-					) {
-						setIsMouseOver(true);
-					} else if (!isMouseOver) {
-						setIsMouseOver(false);
-					}
-				});
-		}
-	}, [current, isMouseOver, onlyHover, photoElement]);
 
 	const { style } = useFloatingAnimation(animationOptions);
 
 	return (
 		<img
-			className={`classPhoto ${isMouseOver ? "classPhoto--mouseover" : ""} ${
-				hoverClass ? `classPhoto--${hoverClass}` : ""
-			}`}
+			className={`classPhoto ${hoverClass ? `classPhoto--${hoverClass}` : ""}`}
 			ref={photoReference}
 			src={source}
 			alt={alternative}
