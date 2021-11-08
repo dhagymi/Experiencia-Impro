1e5a7febd03df0da791da802b3fdb0e6539aa837
agusand <amandreacchi@gmail.com>
2021-11-08 19:14:36 -0300

    Why section layout testing

--- frontend/src/pages/Why.js
+++ frontend/src/pages/Why.js
@@ -11,20 +11,20 @@ const Why = () => {
 					<WhyListItem
 						dotClassModifier="purple"
 						title="Colaboración"
-						mainText="En improvisacion  trabajamos en equipo, buscamos que se luzca la creacion colectiva. En escena, somos todos uno y estamos para"
-						finalText="ayudar a quien lo necesite."
+						mainText="En improvisacion  trabajamos en equipo, buscamos que se luzca la creacion colectiva. En escena, somos todos uno y estamos para ayudar a"
+						finalText="quien lo necesite."
 					/>
 					<WhyListItem
 						dotClassModifier="pink"
 						title="Creatividad"
-						mainText="Empujamos los limites de nuestra imaginacion lo mas lejos posible: en escena, las unicas barreras"
-						finalText="las ponemos nosotros mismos."
+						mainText="Empujamos los limites de nuestra imaginacion lo mas lejos posible: en escena, las unicas barreras las ponemos"
+						finalText="nosotros mismos."
 					/>
 					<WhyListItem
 						dotClassModifier="blue"
 						title="Integración"
-						mainText="Improvisar no es lo único: nos gusta pasarla bien y que el grupo termine en una sala de ensayo, sino que permita  sus participantes encontrar"
-						finalText="un grupo de pertenencia."
+						mainText="Improvisar no es lo único: nos gusta pasarla bien y que el grupo termine en una sala de ensayo, sino que permita sus participantes encontrar un"
+						finalText="grupo de pertenencia."
 					/>
 				</ul>
 			</div>
